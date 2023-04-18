4f2edf3c9f7b8c5c858b02efd86b0f7dbdc23d4e
Robin Shi <robinshi@outlook.com>
2023-04-18 13:02:04 +0800

    remove okex info

--- cli/test.py
+++ cli/test.py
@@ -45,35 +45,6 @@ def main(env: str):
     print(config.get_symbol_datas("APT/USDT"))
     print(config.get_symbol_data_by_makeonly("APT/USDT", "okex"))
 
-    exchanges = {
-        "okex": ccxt.okex(
-            {
-                "apiKey": "593356d6-0726-42c3-a138-f04a3c0449d6",
-                "secret": "2F084373ED1C25FB91705FFF155DBB60",
-                "password": "Okex@2023",
-            }
-        ),
-        "binance": ccxt.binanceusdm(),
-    }
-    print(get_symbol_min_amount(exchanges, "BTC/USDT"))
-
-    okex = exchanges["okex"]
-    okex.load_markets()
-
-    symbol = "BTC/USDT:USDT"
-    symbol_info = okex.market(symbol)
-    qty = 0.023
-    qty = qty / symbol_info["contractSize"]
-    order_qty = okex.amount_to_precision(symbol, qty)
-    # symbol_info
-    float(order_qty) * symbol_info["contractSize"]
-    order = okex.fetch_order(566671904145698830, "FTM/USDT:USDT")
-    res = normalize_ccxt_order(order, "okex")
-    print(res.dict())
-
-    get_exchange_name(okex)
-    print(_get_order(okex, "FTM/USDT", 566671904145698830))
-
 
 if __name__ == "__main__":
     main()
