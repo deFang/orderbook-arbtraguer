5bdd7136385e5007361375496cfc06efc44a97bd
Hugefiver <>
2023-04-20 11:01:07 +0800

    fix

--- cross_arbitrage/order/position_status.py
+++ cross_arbitrage/order/position_status.py
@@ -102,37 +102,39 @@ def align_position(rc: redis.Redis, exchanges: dict[str, ccxt.Exchange], symbols
             continue
         else:
             unprocessed_symbol_list.append(symbol)
-        refresh_position_status(rc, exchanges, unprocessed_symbol_list)
-        for symbol in unprocessed_symbol_list:
-            try:
-                positions = []
-                for exchange_name in exchanges.keys():
-                    position = get_position_status(rc, exchange_name, symbol)
-                    if position == None:
-                        position = PositionStatus(
-                            direction=PositionDirection.long, qty=Decimal(0))
-                    positions.append((exchange_name, position))
-                delta = positions[0][1].qty - positions[1][1].qty
-                min_qty = get_symbol_min_amount(exchanges, symbol)
-                if abs(delta) > min_qty:
-                    logging.info(f"align position: {symbol} {positions}")
-                if delta > min_qty:
-                    exchange = exchanges[positions[0][0]]
-                    side = 'sell' if positions[0][1].direction == PositionDirection.long else 'buy'
-                    market_order(exchange, symbol,
-                                 side, delta,
-                                 client_id=f"{order_prefix}T{int(time.time() * 1000)}")
-                elif delta < -min_qty:
-                    exchange = exchanges[positions[1][0]]
-                    side = 'sell' if positions[1][1].direction == PositionDirection.long else 'buy'
-                    market_order(exchange, symbol,
-                                 side, -delta,
-                                 client_id=f"{order_prefix}T{int(time.time() * 1000)}")
-            except Exception as ex:
-                logging.error(ex)
-                logging.exception(ex)
-            finally:
-                rc.srem('order:signal:processing', symbol)
+
+    refresh_position_status(rc, exchanges, unprocessed_symbol_list)
+    
+    for symbol in unprocessed_symbol_list:
+        try:
+            positions = []
+            for exchange_name in exchanges.keys():
+                position = get_position_status(rc, exchange_name, symbol)
+                if position == None:
+                    position = PositionStatus(
+                        direction=PositionDirection.long, qty=Decimal(0))
+                positions.append((exchange_name, position))
+            delta = positions[0][1].qty - positions[1][1].qty
+            min_qty = get_symbol_min_amount(exchanges, symbol)
+            if abs(delta) > min_qty:
+                logging.info(f"align position: {symbol} {positions}")
+            if delta > min_qty:
+                exchange = exchanges[positions[0][0]]
+                side = 'sell' if positions[0][1].direction == PositionDirection.long else 'buy'
+                market_order(exchange, symbol,
+                                side, delta,
+                                client_id=f"{order_prefix}T{int(time.time() * 1000)}")
+            elif delta < -min_qty:
+                exchange = exchanges[positions[1][0]]
+                side = 'sell' if positions[1][1].direction == PositionDirection.long else 'buy'
+                market_order(exchange, symbol,
+                                side, -delta,
+                                client_id=f"{order_prefix}T{int(time.time() * 1000)}")
+        except Exception as ex:
+            logging.error(ex)
+            logging.exception(ex)
+        finally:
+            rc.srem('order:signal:processing', symbol)
 
 
 def align_position_loop(ctx: CancelContext, rc: redis.Redis, exchanges: dict[str, ccxt.Exchange], symbols: list):
