1cf0f98a084e433acaee185d68df2267a37648e4
Hugefiver <>
2023-05-16 14:58:56 +0800

    add dynamic threshold

--- cross_arbitrage/order/process_threshold.py
+++ cross_arbitrage/order/process_threshold.py
@@ -4,6 +4,8 @@ from decimal import Decimal
 from typing import List
 
 import orjson
+import pandas as pd
+import numpy as np
 import redis
 
 from cross_arbitrage.fetch.fetch_funding_rate import get_funding_rate_key
@@ -13,10 +15,12 @@ from cross_arbitrage.order.position_status import PositionDirection, PositionSta
 from cross_arbitrage.order.threshold import SymbolConfig, ThresholdConfig
 from cross_arbitrage.order.config import SymbolConfig as OrderSymbolConfig
 from cross_arbitrage.utils.context import CancelContext, sleep_with_context
+from cross_arbitrage.utils.decorator import retry
 
 
 def get_threshold_key(exchange):
     return f"order:thresholds:{exchange}"
+
 
 def init_symbol_config(symbol_info: OrderSymbolConfig) -> SymbolConfig:
     return SymbolConfig(
@@ -97,6 +101,7 @@ def _get_threshold_by_funding_delta(ex_name:str, threshold: SymbolConfig, symbol
         logging.info(f"{ex_name} {symbol} {percent} funding_delta={funding_delta} short_threshold={short}")
     return threshold
 
+
 def process_funding_rate(threshold: SymbolConfig, symbol_info: OrderSymbolConfig, exchange_names: List[str], rc: redis.Redis) -> SymbolConfig:
     try:
         now = now_s()
@@ -116,8 +121,10 @@ def process_funding_rate(threshold: SymbolConfig, symbol_info: OrderSymbolConfig
         res = []
         with rc.pipeline() as pipe:
             pipe.multi()
-            pipe.get(get_funding_rate_key(maker_exchange_name, symbol_info.symbol_name))
-            pipe.get(get_funding_rate_key(taker_exchange_name, symbol_info.symbol_name))
+            pipe.get(get_funding_rate_key(
+                maker_exchange_name, symbol_info.symbol_name))
+            pipe.get(get_funding_rate_key(
+                taker_exchange_name, symbol_info.symbol_name))
             res = pipe.execute()
 
         # return if any funding rate is None
@@ -126,7 +133,8 @@ def process_funding_rate(threshold: SymbolConfig, symbol_info: OrderSymbolConfig
         maker_funding_info = orjson.loads(res[0])
         taker_funding_info = orjson.loads(res[1])
 
-        funding_delta = Decimal(maker_funding_info['funding_rate']) - Decimal(taker_funding_info['funding_rate'])
+        funding_delta = Decimal(
+            maker_funding_info['funding_rate']) - Decimal(taker_funding_info['funding_rate'])
 
         if (now % funding_interval) / (60 * 60) <= 6.0:
             threshold = _get_threshold_by_funding_delta(maker_exchange_name, threshold, symbol_info, funding_delta, Decimal('0.33'), max_threshold)
@@ -139,9 +147,75 @@ def process_funding_rate(threshold: SymbolConfig, symbol_info: OrderSymbolConfig
         logging.exception(ex)
     return threshold
 
-def process_orderbook_stat(threshold: SymbolConfig, symbol_info: OrderSymbolConfig, rc: redis.Redis) -> SymbolConfig:
-    # TODO
+
+def process_orderbook_stat(threshold: SymbolConfig, config: OrderConfig, symbol_info: OrderSymbolConfig, rc: redis.Redis,
+                           ob_df: pd.DataFrame, exchange_pair: list[str]) -> SymbolConfig:
+    if exchange_pair[0] == symbol_info.makeonly_exchange_name:
+        maker_ex = 'ex1'
+        taker_ex = 'ex2'
+    elif exchange_pair[1] == symbol_info.makeonly_exchange_name:
+        maker_ex = 'ex2'
+        taker_ex = 'ex1'
+    else:
+        logging.error('maker exchange {} not in exchange pair {}'.format(
+            symbol_info.makeonly_exchange_name, exchange_pair))
+
+    def par_maker(name):
+        return f'{maker_ex}_{name}'
+
+    def par_taker(name):
+        return f'{taker_ex}_{name}'
+
+    df = ob_df[ob_df['symbol'] == symbol_info.symbol_name].sort_values(
+        by='ts', ascending=True)
+    if df.empty:
+        logging.warning('symbol {} orderbook dataframe is empty'.format(symbol_info.symbol_name))
+        return threshold
+    
+    df['delta_bid'] = df[par_maker('bid')] / df[par_taker('ask')] - 1
+    df['delta_ask'] = df[par_maker('ask')] / df[par_taker('bid')] - 1
+
+    bid_mu = df['delta_bid'].mean()
+    bid_sig = df['delta_bid'].std()
+    ask_mu = df['delta_ask'].mean()
+    ask_sig = df['delta_ask'].std()
+
+    long_increase_threshold = Decimal(str(bid_mu - bid_sig * config.dyn_threshold.increase_sigma))
+    long_decrease_threshold = Decimal(str(bid_mu - bid_sig * config.dyn_threshold.decrease_sigma))
+    short_increase_threshold = Decimal(str(ask_mu + ask_sig * config.dyn_threshold.increase_sigma))
+    short_decrease_threshold = Decimal(str(ask_mu + ask_sig * config.dyn_threshold.decrease_sigma))
+
+    if config.debug:
+        logging.info(('\n\n[symbol: {}, maker_exchange: {}] bid_mu: {}, bid_sig: {}, ask_mu: {}, ask_sig: {},'
+                      'long_increase_threshold: {}, long_decrease_threahold: {}, short_increase_threshold: {}, short_decrease_threahold: {}, pre_threshold: {}').format(
+            symbol_info.symbol_name, symbol_info.makeonly_exchange_name,
+            bid_mu, bid_sig, ask_mu, ask_sig,
+            long_increase_threshold, long_decrease_threshold, short_increase_threshold, short_decrease_threshold,
+            threshold))
+
+    if threshold.long_threshold.increase_position_threshold > long_increase_threshold:
+        delta = long_increase_threshold - threshold.long_threshold.increase_position_threshold
+        threshold.long_threshold.increase_position_threshold = long_increase_threshold
+        threshold.long_threshold.cancel_increase_position_threshold += delta
+    if threshold.long_threshold.decrease_position_threshold < long_decrease_threshold:
+        delta = long_decrease_threshold - threshold.long_threshold.decrease_position_threshold
+        threshold.long_threshold.decrease_position_threshold = long_decrease_threshold
+        threshold.long_threshold.cancel_decrease_position_threshold += delta
+    if threshold.short_threshold.increase_position_threshold < short_increase_threshold:
+        delta = short_increase_threshold - threshold.short_threshold.increase_position_threshold
+        threshold.short_threshold.increase_position_threshold = short_increase_threshold
+        threshold.short_threshold.cancel_increase_position_threshold += delta
+    if threshold.short_threshold.decrease_position_threshold > short_decrease_threshold:
+        delta = short_decrease_threshold - threshold.short_threshold.decrease_position_threshold
+        threshold.short_threshold.decrease_position_threshold = short_decrease_threshold
+        threshold.short_threshold.cancel_decrease_position_threshold += delta
+
+    if config.debug:
+        logging.info('\n[symbol: {}, maker_exchange: {}] after process: {}'.format(symbol_info.symbol_name, 
+                                                                                 symbol_info.makeonly_exchange_name,
+                                                                                 threshold))
     return threshold
+
 
 def process_funding_rate_binance_okex_pair(ctx: CancelContext, threshold: SymbolConfig, config: OrderConfig, symbol_info: OrderSymbolConfig, rc: redis.Redis) -> SymbolConfig:
     ex_name = symbol_info.makeonly_exchange_name
@@ -171,12 +245,60 @@ def process_funding_rate_binance_okex_pair(ctx: CancelContext, threshold: Symbol
                             long.increase_position_threshold += delta
                             long.cancel_increase_position_threshold += delta
 
-                    logging.info(f"{symbol_info.makeonly_exchange_name} {symbol_info.symbol_name} funding_delta={funding_info['delta']} threshold={threshold}")
+                    logging.info(
+                        f"{symbol_info.makeonly_exchange_name} {symbol_info.symbol_name} funding_delta={funding_info['delta']} threshold={threshold}")
             return threshold
         except Exception as ex:
-            logging.error(f"process_funding_rate_binance_okex_pair error: {ex}")
+            logging.error(
+                f"process_funding_rate_binance_okex_pair error: {ex}")
             logging.exception(ex)
             return threshold
+
+
+@retry(max_retry_count=3, raise_exception=True, default_return_value=[])
+def get_orderbook(ctx: CancelContext, config: OrderConfig, rc: redis.Redis, symbols: List[str] | set[str]) -> list:
+    orderbooks = []
+    last_id = (now_s() - config.dyn_threshold.time_window_seconds) * 1000
+
+    while not ctx.is_canceled():
+        res = rc.xread({config.redis.orderbook_stream: last_id}, count=1000)
+        if not res:
+            break
+
+        obs = res[0][1]
+        if not obs:
+            break
+        last_id = obs[-1][0]
+        for _id, ob in obs:
+            for symbol, ob in ob.items():
+                if symbol not in symbols:
+                    continue
+                orderbooks.append(orjson.loads(ob))
+    return orderbooks
+
+
+def get_dataframe_from_orderbook(obs: list, exchanges: list[str]):
+    if len(exchanges) != 2:
+        raise ValueError("exchanges length must be 2")
+    ex1, ex2 = exchanges
+    arr = []
+    for ob in obs:
+        if ob['exchange'] not in [ex1, ex2]:
+            continue
+        if not ob.get(ex1, None) or not ob.get(ex2, None)\
+                or not ob[ex1]['bids'] or not ob[ex1]['asks'] \
+                or not ob[ex2]['bids'] or not ob[ex2]['asks']:
+            continue
+        arr.append(
+            (ob['symbol'], np.int64(ob['ts']), ob['exchange'],
+             np.float64(ob[ex1]['bids'][0][0]), np.float64(
+                 ob[ex1]['asks'][0][0]),
+             np.float64(ob[ex2]['bids'][0][0]), np.float64(ob[ex2]['asks'][0][0]),)
+        )
+    df = pd.DataFrame(arr, columns=['symbol', 'ts', 'trigger_exchange',
+                                    'ex1_bid', 'ex1_ask', 'ex2_bid', 'ex2_ask'])
+    return df
+
 
 def process_threshold_mainloop(ctx: CancelContext, config: OrderConfig):
     # init redis client
@@ -188,7 +310,16 @@ def process_threshold_mainloop(ctx: CancelContext, config: OrderConfig):
     def get_taker_exchange_name(maker_exchange_name):
         return (set(config.exchanges) - {maker_exchange_name}).pop()
 
+    symbols_set = set(
+        symbol_info.symbol_name for symbol_info in config.cross_arbitrage_symbol_datas)
+    symbols = list(symbols_set)
+    exchanges = list(config.exchange_pair_names)
+
     while not ctx.is_canceled():
+        orderbook = get_orderbook(ctx, config, rc, symbols_set)
+
+        ob_df = get_dataframe_from_orderbook(orderbook, exchanges)
+
         for symbol_info in config.cross_arbitrage_symbol_datas:
             # init
             threshold = init_symbol_config(symbol_info)
@@ -205,10 +336,12 @@ def process_threshold_mainloop(ctx: CancelContext, config: OrderConfig):
                         f"unknown exchange pair: {maker_exchange_name}, {taker_exchange_name}")
 
             # funding rate
-            threshold = process_funding_rate(threshold, symbol_info, [maker_exchange_name, taker_exchange_name], rc)
+            threshold = process_funding_rate(threshold, symbol_info, [
+                                             maker_exchange_name, taker_exchange_name], rc)
 
             # orderbook stat
-            threshold = process_orderbook_stat(threshold, symbol_info, rc)
+            threshold = process_orderbook_stat(
+                threshold, config, symbol_info, rc, ob_df, exchanges)
 
             if threshold is not None:
                 try:
@@ -217,7 +350,8 @@ def process_threshold_mainloop(ctx: CancelContext, config: OrderConfig):
                         symbol_info.symbol_name,
                         threshold.json_bytes(),)
                 except Exception as ex:
-                    logging.error(f"save threshold error: {ex}, {threshold=}, {maker_exchange_name=}")
+                    logging.error(
+                        f"save threshold error: {ex}, {threshold=}, {maker_exchange_name=}")
                     logging.exception(ex)
 
         sleep_with_context(ctx, 2 * 60, interval=1.0)
